7668b858235fd5c50e4bb82514bc0ee10b3b08aa
claud <claudio104.as@gmail.com>
2026-08-20 13:48:46 -0600

    Mejora de diseño de calculadora(puse colores)

--- Salario/src/main/java/ni/uam/edu/salario/HelloApplication.java
+++ Salario/src/main/java/ni/uam/edu/salario/HelloApplication.java
@@ -16,12 +16,26 @@ public class HelloApplication extends Application {
     public void start(Stage stage) {
 
         Label lblTitulo = new Label("Calculadora de Salario");
-        lblTitulo.setStyle("-fx-font-size: 18px; -fx-font-weight: bold;");
+        lblTitulo.setStyle("-fx-text-fill: red; " +
+                "-fx-font-weight: bold;" +
+                "-fx-font-size: 18px; -fx-font-weight: bold");
+
 
         Label lblPide = new Label("Ingresa el salario del trabajador:");
+        lblPide.setStyle("-fx-text-fill: purple;" +
+                "-fx-font-weight: bold;" +
+                "-fx-font-size: 14px; " +
+                "-fx-font-weight: bold");
         TextField tfSalario = new TextField();
+        tfSalario.setStyle("-fx-text-fill: black;" +
+                "-fx-font-size: 13;" +
+                "-fx-font-weight: bold");
 
         Button btnCalcular = new Button("Calcular");
+        btnCalcular.setStyle("-fx-font-size: 16px;" +
+                "-fx-text-fill: grenn;" +
+                "-fx-background-color: skyblue");
+        btnCalcular.setDefaultButton(true);
 
         Label lblSeguro    = new Label("");
         Label lblBono      = new Label("");
@@ -43,17 +57,28 @@ public class HelloApplication extends Application {
 
             double salarioNeto = salario - seguro + bono;
 
-            lblSeguro.setText("Seguro Social (7%): " +  seguro);
-            lblBono.setText("Bono: " +  bono);
-            lblSalNeto.setText("Salario Neto: " + salarioNeto);
+            lblSeguro.setText("Seguro Social (7%): " + String.format("%.2f", seguro));
+            lblSeguro.setStyle("-fx-text-fill: orange;" +
+                    "-fx-font-size:12px;" +
+                    "-fx-font-weight: bold");
+            lblBono.setText("Bono: " + String.format("%.2f", bono));
+            lblBono.setStyle("-fx-text-fill: yellow;" +
+                    "-fx-font-size:12px;" +
+                    "-fx-font-weight: bold");
+            lblSalNeto.setText("Salario Neto: " + String.format("%.2f", salarioNeto));
+            lblSalNeto.setStyle("-fx-text-fill: blue;" +
+                    "-fx-font-size:12px;" +
+                    "-fx-font-weight: bold");
         });
 
         VBox vbox = new VBox(10);
         vbox.setAlignment(Pos.CENTER);
         vbox.setPadding(new Insets(20));
+        vbox.setStyle("-fx-background-color: black");
         vbox.getChildren().addAll(lblTitulo, lblPide, tfSalario, btnCalcular, lblSeguro, lblBono, lblSalNeto);
 
         Scene scene = new Scene(vbox, 320, 300);
+
         stage.setTitle("Calculadora de Salario");
         stage.setScene(scene);
         stage.show();
